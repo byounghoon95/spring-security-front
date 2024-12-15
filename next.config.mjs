b3e47ce7e72a0722be9899c01754ee580ba60b81
owner <toltolyibh@gmail.com>
2024-12-15 21:53:38 +0900

    프록시 추가

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ const nextConfig = {
       },
     ];
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+        // destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
